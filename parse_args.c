e89ac3485e1c6284044f721c63888f1cd0f41ebb
Ahmed Ouazzani <ahmedouazzani225@gmail.com>
2023-07-25 07:52:58 -0700

    memory leaks fixed

--- parse_args.c
+++ parse_args.c
@@ -18,7 +18,8 @@ char **parse_args(char *input, char *delim)
 	if (tokens == NULL)
 	{
 		perror(": allocation error\n");
-		exit(EXIT_FAILURE);
+		/*exit(EXIT_FAILURE);*/
+		return (NULL);
 	}
 	token = strtok(input, delim);
 	while (token != NULL)
@@ -30,7 +31,8 @@ char **parse_args(char *input, char *delim)
 			for (j = 0; j < i; j++)
 				free(tokens[j]);
 			free(tokens);
-			exit(EXIT_FAILURE);
+			/*exit(EXIT_FAILURE);*/
+			return (NULL);
 		}
 		tokens[i] = new_token;
 		token = strtok(NULL, delim);
@@ -38,13 +40,15 @@ char **parse_args(char *input, char *delim)
 		if (i >= MAX_ARGS_SIZE - 1)
 		{
 			perror(": allocation error\n");
-			exit(EXIT_FAILURE);
+			/*exit(EXIT_FAILURE);*/
+			break;
 		}
 		if (_strlen(new_token) >= MAX_ARGS_SIZE)
 		{
 			perror(": allocation error\n");
 			free(new_token);
-			exit(EXIT_FAILURE);
+			/*exit(EXIT_FAILURE);*/
+			break;
 		}
 	}
 	tokens[i] = NULL;
